a075846297184122d16909f390ffb33495c55bed
Julen <julenuni@gmail.com>
2020-08-15 16:46:03 +0200

    Added widget for add card

--- home_gui.dart
+++ home_gui.dart
@@ -13,7 +13,8 @@ class _HomeGUIState extends State<HomeGUI> {
       appBar: AppBar(
         title: Text("Transport Card of Madrid"),
       ),
-      body: SafeArea(child: Padding(
+      body: SafeArea(
+          child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
@@ -21,19 +22,54 @@ class _HomeGUIState extends State<HomeGUI> {
               width: double.maxFinite,
               height: size.height / 2.7,
               child: Card(
-                child: Center(child: Text("Coming soon", style: Theme.of(context).textTheme.headline5,)),
+                elevation: 3,
+                child: AddCardWidget(),
               ),
             ),
             SizedBox(
               width: double.maxFinite,
               height: size.height / 2.7,
               child: Card(
-                child: Center(child: Text("Coming soon", style: Theme.of(context).textTheme.headline5,)),
+                elevation: 3,
+                child: Center(
+                    child: Text(
+                  "Coming soon",
+                  style: Theme.of(context).textTheme.headline5,
+                )),
               ),
             )
           ],
         ),
       )),
+    );
+  }
+}
+
+class AddCardWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.add_circle,
+              size: 50,
+            ),
+            SizedBox(height: 16,),
+            Text(
+              "Add card",
+              style: Theme.of(context).textTheme.headline4.copyWith(
+                  color: Theme.of(context).brightness ==
+                      Brightness.light
+                      ? Colors.black
+                      : Colors.white),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- main.dart
+++ main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
+        brightness: Brightness.light,
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
