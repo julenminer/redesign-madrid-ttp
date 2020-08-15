d63d40c9f7637b2195e5e6319dd6b556d24b1561
Julen <julenuni@gmail.com>
2020-08-15 16:34:57 +0200

    Add cards to homescreen

--- home_gui.dart
+++ home_gui.dart
@@ -8,6 +8,32 @@ class HomeGUI extends StatefulWidget {
 class _HomeGUIState extends State<HomeGUI> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Transport Card of Madrid"),
+      ),
+      body: SafeArea(child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.maxFinite,
+              height: size.height / 2.7,
+              child: Card(
+                child: Center(child: Text("Coming soon", style: Theme.of(context).textTheme.headline5,)),
+              ),
+            ),
+            SizedBox(
+              width: double.maxFinite,
+              height: size.height / 2.7,
+              child: Card(
+                child: Center(child: Text("Coming soon", style: Theme.of(context).textTheme.headline5,)),
+              ),
+            )
+          ],
+        ),
+      )),
+    );
   }
 }
--- main.dart
+++ main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ttpmadridredesign/home_gui.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,12 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomeGUI(),
     );
   }
 }
