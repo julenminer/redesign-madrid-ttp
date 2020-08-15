928c9ad5ead2b44d0af40196d8008e0b2b323164
Julen <julenuni@gmail.com>
2020-08-15 16:13:09 +0200

    first commit

---
+++ home_gui.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeGUI extends StatefulWidget {
+  @override
+  _HomeGUIState createState() => _HomeGUIState();
+}
+
+class _HomeGUIState extends State<HomeGUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
