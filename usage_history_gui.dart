e9939bb5a2d170255e4291f1f034590cffe6e992
Julen <julenuni@gmail.com>
2020-08-23 13:51:57 +0200

    Created usage history

---
+++ usage_history_gui.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+class UsageHistoryGui extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Usage history"),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView(
+            children: [
+              ListElement(TransportType.cercanias, "Sol", DateTime(2020, 07, 11, 13, 22), "Avenida de América", DateTime(2020, 07, 11, 14, 7)),
+              Divider(),
+              ListElement(TransportType.metro, "Sol", DateTime(2020, 07, 13, 11, 02), "Pacífico", DateTime(2020, 07, 13, 11, 17)),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+enum TransportType {
+  metro,
+  cercanias,
+  bus
+}
+
+class ListElement extends StatelessWidget {
+  ListElement(this.transportType, this.fromName, this.fromDate, this.toName, this.toDate);
+
+  TransportType transportType;
+  String fromName;
+  String toName;
+  DateTime fromDate;
+  DateTime toDate;
+
+  @override
+  Widget build(BuildContext context) {
+    String assetName = "";
+    switch(transportType){
+      case TransportType.metro:
+        assetName = "assets/MetroMadridLogo.png";
+        break;
+      case TransportType.cercanias:
+        assetName = "assets/1024px-Cercanias_Logo.svg.png";
+        break;
+      case TransportType.bus:
+        assetName = "assets/lineas_emt.png";
+        break;
+    }
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Image.asset(assetName, width: MediaQuery.of(context).size.width * 0.07),
+          SizedBox(width: 16,),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                fromName,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(dateString(fromDate)),
+              Icon(Icons.arrow_downward),
+              Text(
+                toName,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(dateString(toDate)),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  String dateString(DateTime date) {
+    String returnString;
+    if(date.day < 10) {
+      returnString = "0"+ date.day.toString();
+    } else {
+      returnString = date.day.toString();
+    }
+    returnString += "/";
+    if(date.month < 10) {
+      returnString += "0"+ date.month.toString();
+    } else {
+      returnString += date.month.toString();
+    }
+    returnString += "/";
+
+    returnString += date.year.toString();
+
+    returnString += " · ";
+
+    if(date.hour < 10) {
+      returnString += "0"+ date.hour.toString();
+    } else {
+      returnString += date.hour.toString();
+    }
+    returnString += ":";
+    if(date.minute < 10) {
+      returnString += "0"+ date.minute.toString();
+    } else {
+      returnString += date.minute.toString();
+    }
+    return returnString;
+  }
+}
